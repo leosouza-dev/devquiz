7de63904d5b63d46a1e3460d9e3648f31252fad7
leosouzadev <leonardolrs1910@gmail.com>
2021-04-21 01:30:50 -0300

    testando e conhecendo a sintaxe do flutter e dart

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,5 @@
-import 'package:DevQuiz/home/home_page.dart';
-import 'package:DevQuiz/splash/splash_page.dart';
+// import 'package:DevQuiz/home/home_page.dart';
+// import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,7 +7,29 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(title: Text(
+          "NLW5 Flutter"),
+          ),
+          body: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(10),       
+              ),
+              child: Text(
+              "Olá, Leo Souza DEV", 
+              style: TextStyle(
+                fontSize: 30, 
+                color: Colors.white,          
+                ),
+              ))),   
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.add),
+          ),
+      ),
+      // home: HomePage(),
     );
   }
 }
