b5baccc0677f18546a601510b77c6773a963a85a
leosouzadev <leonardolrs1910@gmail.com>
2021-04-21 02:06:55 -0300

    criando primeira tela - splash

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,6 @@
 // import 'package:DevQuiz/home/home_page.dart';
 // import 'package:DevQuiz/splash/splash_page.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,29 +8,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: Scaffold(
-        appBar: AppBar(title: Text(
-          "NLW5 Flutter"),
-          ),
-          body: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.purple,
-                borderRadius: BorderRadius.circular(10),       
-              ),
-              child: Text(
-              "Olá, Leo Souza DEV", 
-              style: TextStyle(
-                fontSize: 30, 
-                color: Colors.white,          
-                ),
-              ))),   
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-          child: Icon(Icons.add),
-          ),
-      ),
-      // home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
