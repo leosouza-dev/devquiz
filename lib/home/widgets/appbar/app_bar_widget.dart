7f43f671975662807ed11bccd033cd07a2afd1ba
leosouza-dev <leonardo.89@uol.com.br>
2021-04-21 13:03:24 -0300

    appbar ok

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -5,8 +5,32 @@ class AppBarWidget extends PreferredSize {
   AppBarWidget() : super(
     preferredSize: Size.fromHeight(250),
     child: Container(
-      decoration: BoxDecoration(
-        gradient: AppGradients.linear),
+      height: 250,
+      decoration: BoxDecoration(gradient: AppGradients.linear),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text.rich(TextSpan(text: "Olá, ", 
+            style: AppTextStyles.title, 
+            children: [
+              TextSpan(text: "Leonardo",
+              style: AppTextStyles.titleBold),
+            ],)),
+            Container(
+              height: 58,
+              width: 58,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage("https://avatars.githubusercontent.com/u/44447958?v=4")
+                  ),
+              ),
+              )
+          ],
+          ),
+      ),
     ),
   );
 
